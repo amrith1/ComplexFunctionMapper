98b56c96640f6a9354efe1b2f4ce2c06f0c14fc8
Amrith Lotlikar <amrith.prashant.lotlikar@ibm.com>
2019-07-18 19:03:37 -0500

    animation now works

--- complex_mapper.py
+++ complex_mapper.py
@@ -6,8 +6,8 @@ import time
 xmin = -1 * np.pi
 xmax = np.pi
 xstep = 0.05 * np.pi
-ymin = -4 * np.pi
-ymax = 4 *np.pi
+ymin = 0 #-1 * np.pi
+ymax = 2 *np.pi
 ystep = 0.05 * np.pi
 
 xvisible = int(np.floor((xmax - xmin)/xstep))
@@ -16,16 +16,16 @@ yvisible = int(np.floor((ymax - ymin)/ystep))
 canvasWidth = 1800
 canvasHeight = 1000
 
-displayWindow = [-54000.0, -30000.0, 54000.0, 30000.0]
+displayWindow = [-5.4, -3.0, 5.4, 3.0]
 
-animation_steps = 10
+animation_steps = 100
 
 def tkinterX(x_coord):
 	return int(np.floor((x_coord - displayWindow[0])/(displayWindow[2] - displayWindow[0]) * canvasWidth))
 def tkinterY(y_coord):
 	return canvasHeight - int(np.floor((y_coord - displayWindow[1])/(displayWindow[3] - displayWindow[1]) * canvasHeight))
 def function(value):
-	return np.cos(value)
+	return np.sin(value)
 
 domain = [ [ ((xmin + xval*xstep) + (ymin + yval*ystep)*1j) for yval in range(yvisible + 1) ] for xval in range(xvisible + 1) ]
 intermediate = [ [ ((xmin + xval*xstep) + (ymin + yval*ystep)*1j) for yval in range(yvisible + 1) ] for xval in range(xvisible + 1) ]
@@ -43,30 +43,40 @@ for xval in range(xvisible):
 	canvas_lines.append([])
 	for yval in range(yvisible):
 		canvas_lines[xval].append([])
-		canvas_lines[xval][yval].append(canvas.create_line(tkinterX(output[xval][yval].real), tkinterY(output[xval][yval].imag), tkinterX(output[xval + 1][yval].real), tkinterY(output[xval + 1][yval].imag)))
-		canvas_lines[xval][yval].append(canvas.create_line(tkinterX(output[xval][yval].real), tkinterY(output[xval][yval].imag), tkinterX(output[xval][yval + 1].real), tkinterY(output[xval][yval + 1].imag)))
+		canvas_lines[xval][yval].append(canvas.create_line(tkinterX(domain[xval][yval].real), tkinterY(domain[xval][yval].imag), tkinterX(domain[xval + 1][yval].real), tkinterY(domain[xval + 1][yval].imag)))
+		canvas_lines[xval][yval].append(canvas.create_line(tkinterX(domain[xval][yval].real), tkinterY(domain[xval][yval].imag), tkinterX(domain[xval][yval + 1].real), tkinterY(domain[xval][yval + 1].imag)))
+
 
 while True:
+	master.update()
+	time.sleep(1.0)
 	for step in range(animation_steps):
-		for xval in range(xvisible):
-			for yval in range(yvisible):
+		for xval in range(xvisible + 1):
+			for yval in range(yvisible + 1):
 				intermediate[xval][yval] += change_step[xval][yval]
 		for xval in range(xvisible):
 			for yval in range(yvisible):
-				canvas.coords(canvas_lines[xval][yval], tkinterX(intermediate[xval][yval].real), tkinterY(intermediate[xval][yval].imag), tkinterX(intermediate[xval + 1][yval].real), tkinterY(intermediate[xval + 1][yval].imag))
-				canvas.coords(canvas_lines[xval][yval], tkinterX(intermediate[xval][yval].real), tkinterY(intermediate[xval][yval].imag), tkinterX(intermediate[xval][yval + 1].real), tkinterY(intermediate[xval][yval + 1].imag))
+				canvas.coords(canvas_lines[xval][yval][0], tkinterX(intermediate[xval][yval].real), tkinterY(intermediate[xval][yval].imag), tkinterX(intermediate[xval + 1][yval].real), tkinterY(intermediate[xval + 1][yval].imag))
+				canvas.coords(canvas_lines[xval][yval][1], tkinterX(intermediate[xval][yval].real), tkinterY(intermediate[xval][yval].imag), tkinterX(intermediate[xval][yval + 1].real), tkinterY(intermediate[xval][yval + 1].imag))
 		master.update()
-		time.sleep(0.002)
+		time.sleep(0.02)
 
 		
 	for xval in range(xvisible):
 		for yval in range(yvisible):
-			canvas.coords(canvas_lines[xval][yval], tkinterX(output[xval][yval].real), tkinterY(output[xval][yval].imag), tkinterX(output[xval + 1][yval].real), tkinterY(output[xval + 1][yval].imag))
-			canvas.coords(canvas_lines[xval][yval], tkinterX(output[xval][yval].real), tkinterY(output[xval][yval].imag), tkinterX(output[xval][yval + 1].real), tkinterY(output[xval][yval + 1].imag))
+			canvas.coords(canvas_lines[xval][yval][0], tkinterX(output[xval][yval].real), tkinterY(output[xval][yval].imag), tkinterX(output[xval + 1][yval].real), tkinterY(output[xval + 1][yval].imag))
+			canvas.coords(canvas_lines[xval][yval][1], tkinterX(output[xval][yval].real), tkinterY(output[xval][yval].imag), tkinterX(output[xval][yval + 1].real), tkinterY(output[xval][yval + 1].imag))
 	master.update()
-	time.sleep(1.0)
+	time.sleep(5.0)
 	intermediate = [ [ ((xmin + xval*xstep) + (ymin + yval*ystep)*1j) for yval in range(yvisible + 1) ] for xval in range(xvisible + 1) ]
-
-
-#master.mainloop()
-
+	for xval in range(xvisible):
+		for yval in range(yvisible):
+			canvas.coords(canvas_lines[xval][yval][0], tkinterX(intermediate[xval][yval].real), tkinterY(intermediate[xval][yval].imag), tkinterX(intermediate[xval + 1][yval].real), tkinterY(intermediate[xval + 1][yval].imag))
+			canvas.coords(canvas_lines[xval][yval][1], tkinterX(intermediate[xval][yval].real), tkinterY(intermediate[xval][yval].imag), tkinterX(intermediate[xval][yval + 1].real), tkinterY(intermediate[xval][yval + 1].imag))
+			
+			
+			
+			
+			
+			
+			
